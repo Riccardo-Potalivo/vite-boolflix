3acb511907e40862519308d4ff862ad710e4e98e
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-15 15:26:37 +0100

    navbar-set

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
+  <header>
+    <NavbarComponent/>
+  </header>
 
-  </div>
 </template>
 
 <script>
+  import NavbarComponent from './components/NavbarComponent.vue'
   export default {
+    components: {
+      NavbarComponent
+    }
     
   }
 </script>
 
 <style lang="scss" scoped>
+@use './assets/styles/partials/variables' as *;
+
+header {
+  background-color: $BgDark;
+}
 
 </style>
